7937399c24ae16c1b9e973dec43bbb2a35afa8b6
Fraser Scott <fraser.scott@gmail.com>
2015-08-25 11:22:33 +0100

    only set attr if nil

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default.chef_vault_users.databag = 'chef_vault_users'
 default.chef_vault_users.default_shell = '/bin/bash'
 
-default.users = {}
-default.groups = {}
+default.users ||= {}
+default.groups ||= {}
 
